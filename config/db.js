7f8d5e557fd34a2b15b757e1ca90bce720dfad17
aneeshb123plr <aneeshb123plr@gmail.com>
2019-12-15 14:41:12 +0530

    Adding routes and mongodb connection

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const config = require("config");
+const mongoose = require("mongoose");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true 
+        });
+        console.log("MongoDB connected"); 
+       
+     
+
+    } catch (err) {
+        // Exit process with failure
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;

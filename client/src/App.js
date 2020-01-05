276c934772f2e80506a562b1e2912e3b8ba7dfed
aneeshb123plr <aneeshb123plr@gmail.com>
2020-01-05 14:15:15 +0530

    Added react routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,25 @@ import React,{ Fragment } from 'react';
 import './App.css';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 const App = () => {
   return (
+    <Router>
     <Fragment>
      <Navbar/>
-     <Landing/>
+      <Route exact path="/" component = {Landing}/>
+       <section className="container">
+        <Switch>
+          <Route exact path="/login" component={ Login }/>
+          <Route exact path="/register" component={ Register }/>
+        </Switch>
+  
+       </section>
     </Fragment>
+    </Router>
   );
 }
 
